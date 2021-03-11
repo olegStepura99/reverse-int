fb59c90969cfac7d09d89e5b301e052fe0447dd5
olegStepura99 <79783293+olegStepura99@users.noreply.github.com>
2021-03-11 17:43:56 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+        n = String(n);
+    let newN ='';
+    for(let i =n.length; i > 0; i--){
+        newN+=n[i-1];
+    }
+    return parseInt(newN);
 }
